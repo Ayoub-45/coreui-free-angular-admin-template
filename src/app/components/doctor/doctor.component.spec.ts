7369a62acc575b5b9a86fa96f40defd493a5a4a4
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-05-02 15:05:53 +0100

    add a doctor managment

--- src/app/components/doctor/doctor.component.spec.ts
+++ src/app/components/doctor/doctor.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UpdatePatientComponent } from './update-patient.component';
+import { DoctorComponent } from './doctor.component';
 
-describe('UpdatePatientComponent', () => {
-  let component: UpdatePatientComponent;
-  let fixture: ComponentFixture<UpdatePatientComponent>;
+describe('DoctorComponent', () => {
+  let component: DoctorComponent;
+  let fixture: ComponentFixture<DoctorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UpdatePatientComponent]
+      imports: [DoctorComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(UpdatePatientComponent);
+    fixture = TestBed.createComponent(DoctorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
